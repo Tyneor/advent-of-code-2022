f1b93863bcc7d6bc6ca687d79ba4943a77917489
Tyneor <etienne.quarez@gmail.com>
2022-12-18 00:18:31 +0100

    updated test for part 2 of day 6

--- src/day6/index.test.ts
+++ src/day6/index.test.ts
@@ -2,25 +2,31 @@ import { describe, expect, test } from "vitest";
 import day from "./index.js";
 
 describe("first part", () => {
-  test("mjqjpqmgbljsphdztnvjfqwrcgsmlb", () => {
-    expect(day.firstSolve("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toBe(7);
-  });
-  test("bvwbjplbgvbhsrlpgdmjqwftvncz", () => {
-    expect(day.firstSolve("bvwbjplbgvbhsrlpgdmjqwftvncz")).toBe(5);
-  });
-  test("nppdvjthqldpwncqszvftbrmjlhg", () => {
-    expect(day.firstSolve("nppdvjthqldpwncqszvftbrmjlhg")).toBe(6);
-  });
-  test("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", () => {
-    expect(day.firstSolve("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).toBe(10);
-  });
-  test("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", () => {
-    expect(day.firstSolve("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toBe(11);
-  });
+  const map = {
+    mjqjpqmgbljsphdztnvjfqwrcgsmlb: 7,
+    bvwbjplbgvbhsrlpgdmjqwftvncz: 5,
+    nppdvjthqldpwncqszvftbrmjlhg: 6,
+    nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: 10,
+    zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: 11,
+  };
+  Object.entries(map).forEach(([message, marker]) =>
+    test(message, () => {
+      expect(day.firstSolve(message)).toBe(marker);
+    })
+  );
 });
 
 describe("first part", () => {
-  test("mjqjpqmgbljsphdztnvjfqwrcgsmlb", () => {
-    expect(day.secondSolve("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toBe(0);
-  });
+  const map = {
+    mjqjpqmgbljsphdztnvjfqwrcgsmlb: 19,
+    bvwbjplbgvbhsrlpgdmjqwftvncz: 23,
+    nppdvjthqldpwncqszvftbrmjlhg: 23,
+    nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: 29,
+    zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: 26,
+  };
+  Object.entries(map).forEach(([message, marker]) =>
+    test(message, () => {
+      expect(day.secondSolve(message)).toBe(marker);
+    })
+  );
 });
