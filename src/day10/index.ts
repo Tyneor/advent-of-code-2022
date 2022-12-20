e28cbc292788b712917fe235beb2e577aef5d7fb
Tyneor <etienne.quarez@gmail.com>
2022-12-20 01:36:30 +0100

    finished day10

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -1,10 +1,17 @@
-import { sum } from "../shared/index.js";
+import { chunk, sum } from "../shared/index.js";
 
 const fillSignalStrengths = (strengths: number[], cycle: number, registerX: number) => {
   const interestingCycles = Array.from({ length: 6 }, (_, i) => i * 40 + 20);
   for (let index = 0; index < interestingCycles.length; index++) {
     if (strengths.length === index && cycle >= interestingCycles[index]) strengths.push(interestingCycles[index] * registerX);
   }
+};
+
+const constructScreen = (oneDimScreen: boolean[]) => {
+  return chunk(
+    oneDimScreen.map((bool) => (bool ? "#" : ".")),
+    40
+  ).map((line) => line.join(""));
 };
 
 export default {
@@ -24,6 +31,27 @@ export default {
   },
 
   secondSolve: (input) => {
-    return "0";
+    const lines = input.split("\n");
+    let cycle = 0;
+    let registerX = 1;
+    const oneDimScreen: boolean[] = [];
+
+    const runOneCycle = () => {
+      oneDimScreen.push(Math.abs(cycle - registerX) <= 1);
+      cycle = (cycle + 1) % 40;
+    };
+
+    lines.forEach((line) => {
+      const [operation, value] = line.split(" ");
+      if (operation === "noop") {
+        runOneCycle();
+      } else if (operation === "addx") {
+        runOneCycle();
+        runOneCycle();
+        registerX += parseInt(value);
+      }
+    });
+
+    return constructScreen(oneDimScreen);
   },
-} satisfies Day<number, string>;
+} satisfies Day<number, string[]>;
